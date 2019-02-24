55d89410d00dc916d07c7ab9b04e839c067dd202
EC2 Default User <ec2-user@ip-172-31-33-40.us-east-2.compute.internal>
2019-02-24 22:32:28 +0000

    Part 2 Happy RSpec + Cucumber

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -3,5 +3,10 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
   def self.find_with_same_director(id)
+    movie = Movie.find(id)
+    if not movie.director.nil?
+      same_movies = Movie.where(:director => movie.director)
+    end
+    [movie, same_movies]
   end
 end
--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -13,7 +13,7 @@ describe MoviesController do
       get :search_directors, {:id => 1}
       expect(response).to render_template('search_directors')
     end
-    it 'makes the movies with same director search results available to that template' do
+    it 'makes the movies available to that template' do
       fake_movie = double('Movie')
       fake_same_movies = [double('Movie1'), double('Movie2')]
       #Movie.stub(:find_with_same_director).and_return([fake_movie, fake_same_movies])
